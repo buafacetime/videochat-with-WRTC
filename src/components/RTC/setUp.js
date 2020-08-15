856871a299a9a8d5d74f31e445a7ed33eb9e5c93
Kebalepile <pimpabutterfly86@gmail.com>
2020-08-15 17:19:03 +0200

    added RTCsetup code

--- src/components/RTC/setUp.js
+++ src/components/RTC/setUp.js
@@ -1,50 +1,177 @@
 "user strict";
 import Signaling from "./signaling.js";
 import {
-   stunServers,
-   turnServers 
+    stunServers,
+    turnServers
 } from "./servers/RTCServers.js";
 
 class RTCSETUP extends Signaling {
-    constructor(){
+    constructor(localVideoRef, remoteVideoRef) {
         super();
         this.peer = new RTCPeerConnection({
-            iceServers:[
+            iceServers: [
                 ...stunServers(),
                 ...turnServers()
             ]
         });
- 
+
+        this.localVideoRef = localVideoRef;
+        this.remoteVideoRef = remoteVideoRef;
+
+        // this.setUpEvents();
+
+    }
+
+    async startVideoChat() {
+        this.peer.addEventListener('negotiationneeded', async event => {
+            try {
+                let offerSDP = await this.peer.createOffer({
+                    //offer constraints
+                    offerToReceiveVideo: 1,
+                    offerToReceiveAudio: 1
+                });
+
+                this.peer.setLocalDescription(offerSDP);
+
+                this.send({ desc: offerSDP });
+
+            } catch (err) {
+                console.error(err.toString());
+            }
+        });
+
+        let stream = await this.videoStream();
+
+        for (let track of stream.getTracks()) this.peer.addTrack(track, stream);
+
+    }
+
+    // needs some fixing
+    endVideoChat() {
+
+        this.peer.close();
+        this.peer = new RTCPeerConnection({
+            iceServers: [
+                ...stunServers(),
+                ...turnServers()
+            ]
+        });
     }
 
     /**
      * @description adds relevent event listeners needed
      *  by both peers.
      */
-    setUpEvents (){
+    setUpEvents() {
 
-          this.peer.addEventListener('icecandidate', event => {
-            try{
-                   if(event.candidate) this.send({candidate: event.candidate});
-            }catch (err) {
+        this.peer.addEventListener('icecandidate', event => {
+            try {
+                if (event.candidate) this.send({ candidate: event.candidate });
+            } catch (err) {
                 console.log(`failed to add ICE Candidate: ${err.toString()}`);
             }
-          });
+        });
 
-          this.peer.addEventListener('track', event => this.gotRemoteStream(event));
+        this.peer.addEventListener('track', event => this.addRemoteStream(event));
 
-          this.onmessage(async ({desc, candidate}) => {
-              try{
-                  if(desc){
+        this.onmessage(async ({ desc, candidate }) => {
+            try {
+                if (desc) {
+                    switch (desc.type) {
+                        case "offer":
+                            this.answerToOffer(desc);
+                            break
+                        case "answer":
+                            this.peer.setRemoteDescription(desc);
+                            break
+                    }
 
-                  }else if (candidate){
-                      al
-                  }
+                } else if (candidate) {
+                    await this.peer.addIceCandidate(candidate);
+                }
 
-              }catch (err) {
+            } catch (err) {
                 console.error(err.toString())
-              }
-          })
+            }
+        })
+    }
+    /**
+     * @description gets remote video stream 
+     * @returns stream object
+     */
+    async videoStream() {
+        try {
+            let stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+            return stream
+        }
+        catch (err) {
+            console.error(err.toString());
+        }
+    }
+    /**
+    * @param {object} stream
+    *@description adds local video stream capured to localvideo ref.
+    */
+    addLocalStream(stream) {
+        // this.localVideoRef.addEventListener('loadedmetadata', () => {
+        //     this.localVideoRef.play();
+
+        // });
+
+        this.localVideoRef.srcObject = stream;
+    }
+    /**
+    * @param {Event} event
+    * @description adds received video stream to remotevideo ref
+    */
+    addRemoteStream(event) {
+        let { streams } = event;
+        try {
+            // this.remoteVideoRef.addEventListener('loadedmetadata', () => {
+            //     this.remoteVideoRef.play();
+
+            // });
+            if (this.remoteVideoRef.srcObject !== streams[0]) {
+                this.remoteVideoRef.srcObject = streams[0];
+            }
+
+        } catch (err) {
+            console.error(err.toString());
+        }
+        /**
+     * 
+     * @param {Object} description 
+     * @description
+     * 
+     *  1, Gets video stream of callee, adds it to callee's this.peer.
+     * 
+     * 2, sets remote description with desctiption offer recived from caller.
+     * 
+     * 3, Creates answer, sets localDescripiton with answer description.
+     * 
+     * 4, sends answer description to caller
+     * 
+     */
+    }
+    async answerToOffer(desc) {
+        try {
+            let stream = await this.videoStream();
+
+            for (let track of stream.getTracks()) this.peer.addTrack(track, stream);
+
+            await this.peer.setRemoteDescription(desc);
+
+            let answerSDP = await this.peer.createAnswer();
+
+            await this.peer.setLocalDescription(answerSDP);
+
+            this.send({ desc: answerSDP });
+
+
+        } catch (err) {
+            console.error(err.toString())
+        }
+
     }
 }
 
