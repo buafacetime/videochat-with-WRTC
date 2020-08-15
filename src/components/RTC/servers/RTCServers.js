043bfa18c608938ee5496936c5a1bee0107945ef
Kebalepile <pimpabutterfly86@gmail.com>
2020-08-15 17:06:07 +0200

    add stun && turn servers code

--- src/components/RTC/servers/RTCServers.js
+++ src/components/RTC/servers/RTCServers.js
@@ -4,9 +4,11 @@ const { stunServers } = require("../../../../../../../learning-webrtc/webrtc_app
  * @returns an Array of public stun servers, used to
  * get peers ip address.
  */
-export function stunServers(){
-    return([
-
+export function stunServers() {
+    return ([
+        { url: 'stun:stun1.l.google.com:19302' },
+        { url: 'stun:stun2.l.google.com:19302' },
+        { url: 'stun:stun3.l.google.com:19302' }
     ])
 }
 
@@ -15,8 +17,18 @@ export function stunServers(){
  * used to rely communication if peers cannot
  * communicate directly.
  */
-export function turnServers(){
-    return([
+export function turnServers() {
+    return ([
+        {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com'
+        },
 
+        {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808'
+        }
     ])
 }
