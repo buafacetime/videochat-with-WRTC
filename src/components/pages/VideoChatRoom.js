ea95888453e5830a8db5f6732ce2b0fe63fe7072
Kebalepile <pimpabutterfly86@gmail.com>
2020-08-15 15:39:19 +0200

    added videoChatRoom styles

--- src/components/pages/VideoChatRoom.js
+++ src/components/pages/VideoChatRoom.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import camera from "../../svg/switch_cameras.svg"
 import screenOff from "../../svg/screen_off.svg"
 import screenOn from "../../svg/screen_on.svg"
@@ -8,23 +8,104 @@ import endCall from "../../svg/call_ended.svg"
 import callEnded from "../../svg/end_call.svg"
 
 const VideoChatRoom = () => {
-    const Click = e => {
-        alert(`${e.target.name} clicked`)
-    }
+    const [state, setState] = useState({
+        switchcamera: {
+            ref: useRef(null), boolean: true, off: camera, on: camera, task(bool) {
+                switch (bool) {
+                    case true:
+                        console.log("camera switched")
+                        break
+                    case false:
+                        console.log("camera not switched")
+                        break
+                }
+            }
+        },
+        screen: {
+            ref: useRef(null), boolean: true, off: screenOff, on: screenOn, task(bool) {
+                switch (bool) {
+                    case true:
+                        console.log("screen turned off")
+                        break
+                    case false:
+                        console.log("screen turned on")
+                        break
+                }
+            }
+        },
+        audio: {
+            ref: useRef(null), boolean: true, off: audioOff, on: audioOn, task(bool) {
+                switch (bool) {
+                    case true:
+                        console.log("audio turned off")
+                        break
+                    case false:
+                        console.log("audio turned on")
+                        break
+                }
+            }
+        },
+        endcall: {
+            ref: useRef(null), boolean: true, off: callEnded, on: endCall, task(bool) {
+                switch (bool) {
+                    case true:
+                        console.log("call ended")
+                        break
+                    case false:
+                        console.log("call not ended")
+                        break
+                }
+            }
+        }
+    }), {
+        switchcamera,
+        screen,
+        audio,
+        endcall
+    } = state,
+        /**
+         * @param {object} object
+         * @description Changes source of img clicked and
+         *  invokes task method on object param passed in.
+         */
+        InvokeProcedure = (object) => {
+
+            object.boolean === true ?
+                (object.ref.current.src = object.off) :
+                (object.ref.current.src = object.on);
+
+            object.task(object.boolean);
+
+            object.boolean === true ? (object.boolean = false) : (object.boolean = true);
+        },
+        /**
+         * @description a listener function for an onclick event.
+         */
+        Click = e => {
+            let { name } = e.target;
+            if (name !== 'endcall') {
+                InvokeProcedure(state[name]);
+            } else {
+                InvokeProcedure(state[name]);
+            }
+        }
+
     return (
         <div>
-            <video id="localstream" mute></video>
+            <video id="localstream" mute="true"></video>
             <video id="remotestream"></video>
             <section id="controls">
-                <img src={camera} name="switchcamera" alt="switchcamera" id="switchcamera"
-                    onClick={Click}
-                />
+                <img src={switchcamera['on']} name="switchcamera" alt="switchcamera" id="switchcamera"
+                    ref={switchcamera['ref']} onClick={Click} />
 
-                <img src={screenOff} name="screen" alt="screen" id="screen" onClick={Click} />
+                <img src={screen['on']} name="screen" alt="screen" id="screen"
+                    ref={screen['ref']} onClick={Click} />
 
-                <img src={audioOff} name="audio" alt="audio" id="audio" onClick={Click} />
+                <img src={audio['on']} name="audio" alt="audio" id="audio"
+                    ref={audio['ref']} onClick={Click} />
 
-                <img src={endCall} name="endcall" alt="endcall" id="endcall" onClick={Click} />
+                <img src={endcall['on']} name="endcall" alt="endcall" id="endcall"
+                    ref={endcall['ref']} onClick={Click} />
             </section>
         </div>
     )
