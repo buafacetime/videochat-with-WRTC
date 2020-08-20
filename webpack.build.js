42a382b568caebf46452105f583438f83b616f10
Kebalepile <pimpabutterfly86@gmail.com>
2020-08-20 16:33:26 +0200

    add webmanifest && generate serviceworker plugins

--- webpack.build.js
+++ webpack.build.js
@@ -1,20 +1,18 @@
 const webpack = require("webpack"),
   path = require("path"),
   HTMLWebpackPlugin = require("html-webpack-plugin"),
-  { CleanWebpackPlugin } = require("clean-webpack-plugin");
+  { CleanWebpackPlugin } = require("clean-webpack-plugin"),
+  WebpackPwaManifest = require("webpack-pwa-manifest"),
+  { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = {
   mode: "production",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+  },
   module: {
     rules: [
-      {
-        test: /\.ttf$/,
-        use: [
-          {
-            loader: "ttf-loader",
-          },
-        ],
-      },
       {
         test: /\.(png|jpe?g)$/,
         use: "file-loader",
@@ -63,6 +61,43 @@ module.exports = {
         keywords: "Bua Facetime, K.T motshoana, webrtc facetime",
       },
       favicon: path.resolve("src/favicons/favicon.png"),
+    }),
+    new WebpackPwaManifest({
+      short_name: "Bua FaceTime",
+      name: "Bua FaceTime",
+      description: "a secured video chat in the browser",
+      author: "K.T Motshoana",
+      lang: "en-US",
+      start_url: ".",
+      background_color: "#222",
+      theme_color: "#ff9b04",
+      scope: "/",
+      display: "standalone",
+      icons: [
+        {
+          src: path.resolve("src/icons/144x144.png"),
+          type: "image/png",
+          sizes: "144x144",
+          purpose: "any",
+        },
+        {
+          src: path.resolve("src/icons/192x192.png"),
+          type: "image/png",
+          sizes: "192x192",
+          purpose: "any",
+        },
+        {
+          src: path.resolve("src/icons/512x512.png"),
+          type: "image/png",
+          sizes: "512x512",
+          purpose: "any",
+        },
+      ],
+    }),
+    new GenerateSW({
+      swDest: "sw.js",
+      clientsClaim: true,
+      skipWaiting: true,
     }),
   ],
 };
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,9 @@
 const webpack = require("webpack"),
   path = require("path"),
   HTMLWebpackPlugin = require("html-webpack-plugin"),
-  { CleanWebpackPlugin } = require("clean-webpack-plugin");
+  { CleanWebpackPlugin } = require("clean-webpack-plugin"),
+  WebpackPwaManifest = require("webpack-pwa-manifest"),
+  { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -17,7 +19,6 @@ module.exports = {
   },
   module: {
     rules: [
-     
       {
         test: /\.(png|jpe?g|tff)$/,
         use: "file-loader",
@@ -68,9 +69,48 @@ module.exports = {
         description: "webapp facetime with built in voice effects",
         robots: "index,follow",
         googlebot: "index,follow",
-        keywords: "Bua Facetime, K.T motshoana, webrtc facetime",
+        keywords:
+          "Bua Facetime, K.T motshoana, webrtc facetime, video chat web app javascript",
       },
+
       favicon: path.resolve("src/favicons/favicon.png"),
+    }),
+    new WebpackPwaManifest({
+      short_name: "Bua FaceTime",
+      name: "Bua FaceTime",
+      description: "a secured video chat in the browser",
+      author: "K.T Motshoana",
+      lang: "en-US",
+      start_url: ".",
+      background_color: "#222",
+      theme_color: "#ff9b04",
+      scope: "/",
+      display: "standalone",
+      icons: [
+        {
+          src: path.resolve("src/icons/144x144.png"),
+          type: "image/png",
+          sizes: "144x144",
+          purpose: "any",
+        },
+        {
+          src: path.resolve("src/icons/192x192.png"),
+          type: "image/png",
+          sizes: "192x192",
+          purpose: "any",
+        },
+        {
+          src: path.resolve("src/icons/512x512.png"),
+          type: "image/png",
+          sizes: "512x512",
+          purpose: "any",
+        },
+      ],
+    }),
+    new GenerateSW({
+      swDest: "sw.js",
+      clientsClaim: true,
+      skipWaiting: true,
     }),
   ],
 };
